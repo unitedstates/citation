f478668d9d3faa99869bfc0e7ce047b89c314475
Eric Mill <konklone@gmail.com>
2012-10-06 17:46:20 -0400

    Added a top-level law ID for public law citations, separate from section-level specific IDs

--- test/law.js
+++ test/law.js
@@ -47,7 +47,7 @@ exports.testPatterns = function(test) {
       "Nuclear Energy Authorization Act of 1980 (Private Law 96–164; "],
   ];
 
-  test.expect(cases.length * 6);
+  test.expect(cases.length * 7);
 
   for (var i=0; i<cases.length; i++) {
     var details = cases[i];
@@ -60,6 +60,7 @@ exports.testPatterns = function(test) {
       var citation = found[0];
       test.equal(citation.match, details[0]);
       test.equal(citation.law.id, details[1]);
+      test.equal(citation.law.law_id, details[1]);
       test.equal(citation.law.type, details[2]);
       test.equal(citation.law.congress, details[3]);
       test.equal(citation.law.number, details[4]);
@@ -72,23 +73,23 @@ exports.testPatterns = function(test) {
 exports.testSubsections = function(test) {
   var cases = [
     // variations on text of http://www.gpo.gov/fdsys/pkg/BILLS-112hr6567ih/xml/BILLS-112hr6567ih.xml
-    ["Section 4402 of Public Law 107–171", "public_law_107_171_4402", 
+    ["Section 4402 of Public Law 107–171", "public_law_107_171_4402", "public_law_107_171",
       "public", "107", "171", ["4402"],
       "(3) Section 4402 of Public Law 107–171 (relating"],
-    ["Section 4402(e) of PL 107–171", "public_law_107_171_4402_e", 
+    ["Section 4402(e) of PL 107–171", "public_law_107_171_4402_e", "public_law_107_171",
       "public", "107", "171", ["4402", "e"],
       "(3) Section 4402(e) of PL 107–171 (relating"],
-    ["Section 4402(e)(1) of Public Law 107–171", "public_law_107_171_4402_e_1", 
+    ["Section 4402(e)(1) of Public Law 107–171", "public_law_107_171_4402_e_1", "public_law_107_171",
       "public", "107", "171", ["4402", "e", "1"],
       "(3) Section 4402(e)(1) of Public Law 107–171 (relating"]
   ];
 
-  test.expect(cases.length * 7);
+  test.expect(cases.length * 8);
 
   for (var i=0; i<cases.length; i++) {
     var details = cases[i];
 
-    var text = details[6];
+    var text = details[7];
     var found = Citation.find(text);
     test.equal(found.length, 1, "No match found in: " + text);
 
@@ -96,10 +97,11 @@ exports.testSubsections = function(test) {
       var citation = found[0];
       test.equal(citation.match, details[0]);
       test.equal(citation.law.id, details[1]);
-      test.equal(citation.law.type, details[2]);
-      test.equal(citation.law.congress, details[3]);
-      test.equal(citation.law.number, details[4]);
-      test.deepEqual(citation.law.sections, details[5]);
+      test.equal(citation.law.law_id, details[2]);
+      test.equal(citation.law.type, details[3]);
+      test.equal(citation.law.congress, details[4]);
+      test.equal(citation.law.number, details[5]);
+      test.deepEqual(citation.law.sections, details[6]);
     }
   }
 
