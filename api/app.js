f97b94829db4cfb98b8af663448248114955ee22
Eric Mill <konklone@gmail.com>
2012-10-06 17:43:46 -0400

    Remove unused view engine

--- api/app.js
+++ api/app.js
@@ -35,8 +35,6 @@ var express = require('express');
 var app = module.exports = express();
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
