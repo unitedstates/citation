cf6dabd2679d9c0dfaf62a7c394602c3885a563d
Eric Mill <konklone@gmail.com>
2012-10-06 17:44:00 -0400

    Add section support for public law citations

--- test/law.js
+++ test/law.js
@@ -68,3 +68,40 @@ exports.testPatterns = function(test) {
 
   test.done();
 };
+
+exports.testSubsections = function(test) {
+  var cases = [
+    // variations on text of http://www.gpo.gov/fdsys/pkg/BILLS-112hr6567ih/xml/BILLS-112hr6567ih.xml
+    ["Section 4402 of Public Law 107–171", "public_law_107_171_4402", 
+      "public", "107", "171", ["4402"],
+      "(3) Section 4402 of Public Law 107–171 (relating"],
+    ["Section 4402(e) of PL 107–171", "public_law_107_171_4402_e", 
+      "public", "107", "171", ["4402", "e"],
+      "(3) Section 4402(e) of PL 107–171 (relating"],
+    ["Section 4402(e)(1) of Public Law 107–171", "public_law_107_171_4402_e_1", 
+      "public", "107", "171", ["4402", "e", "1"],
+      "(3) Section 4402(e)(1) of Public Law 107–171 (relating"]
+  ];
+
+  test.expect(cases.length * 7);
+
+  for (var i=0; i<cases.length; i++) {
+    var details = cases[i];
+
+    var text = details[6];
+    var found = Citation.find(text);
+    test.equal(found.length, 1, "No match found in: " + text);
+
+    if (found.length == 1) {
+      var citation = found[0];
+      test.equal(citation.match, details[0]);
+      test.equal(citation.law.id, details[1]);
+      test.equal(citation.law.type, details[2]);
+      test.equal(citation.law.congress, details[3]);
+      test.equal(citation.law.number, details[4]);
+      test.deepEqual(citation.law.sections, details[5]);
+    }
+  }
+
+  test.done();
+};
