3ad1cebe3113efab2b50f3e06679fd57870a85ab
Eric Mill <konklone@gmail.com>
2012-11-04 14:58:19 -0500

    Fixed tests for VA code parser so far, look for type-specific fields in the type-specific subobject

--- test/us_va.js
+++ test/us_va.js
@@ -1,14 +1,14 @@
 /*
-  Tests for extracting slip law citations from legislation.
-  Each test should link to a real world circumstance where possible.
+  Tests for extracting Virginia Code citations.
 */
 
 var Citation = require('../citation');
 
 
 exports.testPatterns = function(test) {
+  test.expect();
+
   var cases = [
-    // variations on text of http://www.gpo.gov/fdsys/pkg/BILLS-112hr2367ih/xml/BILLS-112hr2367ih.xml
     ['Va. Code Ann. § 19.2-56.2 (2010)', '19.2', '56.2', '2010'],
     ['Va. Code Ann. § 19.2-56.2 (West 2010)', '19.2', '56.2', '2010']
   ];
@@ -18,15 +18,16 @@ exports.testPatterns = function(test) {
 
     var text = details[0];
     var found = Citation.find(text);
-    test.equal(found.length, 1, "No match found in: " + text);
+    test.equal(found.length, 1);
 
     if (found.length == 1) {
       var citation = found[0];
       test.equal(citation.match, details[0]);
-      test.equal(citation.title, details[1]);
-      test.equal(citation.section, details[2]);
-      test.equal(citation.year, details[3]);
-    }
+      test.equal(citation.va_code_ann.title, details[1]);
+      test.equal(citation.va_code_ann.section, details[2]);
+      test.equal(citation.va_code_ann.year, details[3]);
+    } else
+      console.log("No match found in: " + text);
   }
 
   test.done();
