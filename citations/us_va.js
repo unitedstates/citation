9b2e70668683da5461f7fc91949bb018623707a4
MrChadRobinson <chad.robinson@statescape.com>
2012-12-10 13:36:05 -0500

    Update citations/us_va.js
    Including additional citation patterns not present in the code as it currently exists in the file's comments.

--- citations/us_va.js
+++ citations/us_va.js
@@ -5,6 +5,11 @@ Citation.types.va_code_ann = {
   patterns: [
     // Va. Code Ann. § 19.2-56.2 (2010)
     // Va. Code Ann. § 19.2-56.2 (West 2010)
+    // Va. Code Ann. § 57-1
+    // Va. Code Ann. § 57-2.02
+    // Va. Code Ann. § 66-25.1:1
+    // Va. Code Ann. § 63.2-300
+    
     [
       "Va\\.? Code\\.? Ann\\.? §* ?(\\d+\\.\\d+)\\-(\\d+\\.\\d+) \\((West )?([12]\\d{3})\\)",
       function(match) {
