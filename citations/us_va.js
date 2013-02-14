ed78f4ae4a99fa0ff7cc495e94458c1cae1cd5fb
Eric Mill <konklone@gmail.com>
2013-02-14 14:14:32 -0500

    fixed up some regexes to handle a few more suggestions by Chad Robinson

--- citations/us_va.js
+++ citations/us_va.js
@@ -3,15 +3,21 @@ Citation.types.va_code_ann = {
   type: "regex",
 
   patterns: [
+    // tested:
     // Va. Code Ann. § 19.2-56.2 (2010)
     // Va. Code Ann. § 19.2-56.2 (West 2010)
     // Va. Code Ann. § 57-1
     // Va. Code Ann. § 57-2.02
-    // Va. Code Ann. § 66-25.1:1
     // Va. Code Ann. § 63.2-300
+
+    // untested:
+    // Va. Code Ann. § 66-25.1:1
     
     [
-      "Va\\.? Code\\.? Ann\\.? §* ?(\\d+\\.\\d+)\\-(\\d+\\.\\d+) \\((West )?([12]\\d{3})\\)",
+      "Va\\.? Code\\.? Ann\\.?" +
+      "(?:\\s+§+)?" +
+      "\\s+([\\d\\.]+)\\-([\\d\\.]+)" +
+      "(?:\\s+\\((West )?([12]\\d{3})\\))?",
       function(match) {
         return {
           title: match[1],
--- test/us_va.js
+++ test/us_va.js
@@ -9,23 +9,36 @@ exports.testPatterns = function(test) {
   test.expect();
 
   var cases = [
-    ['Va. Code Ann. § 19.2-56.2 (2010)', '19.2', '56.2', '2010'],
-    ['Va. Code Ann. § 19.2-56.2 (West 2010)', '19.2', '56.2', '2010']
+    [ 'standard',
+      'Va. Code Ann. § 19.2-56.2 (2010)', 
+      '19.2', '56.2', '2010'],
+    [ 'standard-west',
+      'Va. Code Ann. § 19.2-56.2 (West 2010)', 
+      '19.2', '56.2', '2010'],
+    [ 'no-year-1',
+      'Va. Code Ann. § 57-1', 
+      '57', '1', null],
+    [ 'no-year-2',
+      'Va. Code Ann. § 57-2.02', 
+      '57', '2.02', null],
+    [ 'no-year-3',
+      'Va. Code Ann. § 63.2-300', 
+      '63.2', '300', null]
   ];
 
   for (var i=0; i<cases.length; i++) {
     var details = cases[i];
 
-    var text = details[0];
+    var text = details[1];
     var found = Citation.find(text);
     test.equal(found.length, 1);
 
     if (found.length == 1) {
       var citation = found[0];
-      test.equal(citation.match, details[0]);
-      test.equal(citation.va_code_ann.title, details[1]);
-      test.equal(citation.va_code_ann.section, details[2]);
-      test.equal(citation.va_code_ann.year, details[3]);
+      test.equal(citation.match, details[1], details[0]);
+      test.equal(citation.va_code_ann.title, details[2]);
+      test.equal(citation.va_code_ann.section, details[3]);
+      test.equal(citation.va_code_ann.year, details[4]);
     } else
       console.log("No match found in: " + text);
   }
