989f9ff4ee3d5dc89344e5c76f49ad1c6f10e833
Alan deLevie <adelevie@gmail.com>
2013-12-14 14:36:33 -0500

    add basic walverine integration plus tests

---
+++ test/judicial.js
@@ -0,0 +1,56 @@
+/*
+  Tests this library's integration with Walverine, which parses case law citations
+*/
+
+var Citation = require('../citation');
+var _ = require('underscore');
+var deepEqual = require('deep-equal');
+
+var singles = [
+  // https://www.courtlistener.com/scotus/5s99/arlington-v-fcc/
+  ["he dissent is correct that United States v. Mead Corp., 533 U. S. 218 (2001), requires that", 
+   "Simple case name",
+    [{
+      match: "United States v. Mead Corp., 533 U. S. 218 (2001), requires that", 
+      judicial: {
+        volume: 533,
+        reporter: 'U.S.',
+        page: 218,
+        lookup_index: 0,
+        canonical_reporter: 'U.S.',
+        extra: null,
+        defendant: 'Mead Corp.',
+        plaintiff: 'United States',
+        court: null,
+        year: 2001,
+        mlz_jurisdiction: 'us;federal;supreme.court',
+        match_url: null,
+        end_idx: 12,
+        cert_order: null,
+        disposition: null,
+        seqID: 0,
+        relations: [ 0 ] 
+      }
+    }]
+  ]
+];
+
+
+singles.forEach(function(single) {
+  exports[single[1]] = function(test) {
+    test.expect();
+
+    var text = single[0];
+    var actual = Citation.find(text).citations;
+    var actual = _.map(actual, function(result) {
+      // Not worried about testing these at the moment.
+      result.judicial = _.omit(result.judicial, ["base_citation", "as_regex", "as_html"]);
+      return result;
+    });
+    var expected = single[2];
+
+    test.ok(deepEqual(actual, expected), "derp");
+    test.done();
+  };
+
+});
