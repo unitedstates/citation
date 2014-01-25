717fbbe47dd42291d6775d9dd640de415275d8ec
Joshua Tauberer <jt@occams.info>
2014-01-25 09:58:27 -0500

    in dc_law, when the context is dc_code, allow "Law XX-YYY" citations without the "DC" prefix

--- citations/dc_law.js
+++ citations/dc_law.js
@@ -10,18 +10,26 @@
     };
   },
 
-  patterns: [
-    // "D.C. Law 111-89"
-    // "DC Law 111-89"
-    {
-      regex:
-        "D\\.?\\s*C\\.?\\s+Law\\s+(?<period>\\d+)\\s?[-–]+\\s?(?<number>\\d+)",
-      processor: function(captures) {
-        return {
-          period: captures.period,
-          number: captures.number
-        };
+  patterns: function(context) {
+    // If the context for this citation is the DC Code, then Law XX-YYY can be assumed
+    // to be a DC law. In other context, require the "DC Law" prefix.
+    var context_regex = "";
+    if (context.source != "dc_code")
+      context_regex = "D\\.?\\s*C\\.?\\s+";
+
+    return [
+      // "D.C. Law 111-89"
+      // "DC Law 111-89"
+      {
+        regex:
+          context_regex + "Law\\s+(?<period>\\d+)\\s?[-–]+\\s?(?<number>\\d+)",
+        processor: function(captures) {
+          return {
+            period: captures.period,
+            number: captures.number
+          };
+        }
       }
-    }
-  ]
+    ];
+  }
 });
