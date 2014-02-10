8377d821aed79fcf84b0b6ea1747243a7d662e2a
vzvenyach <vdavez@gmail.com>
2014-02-09 21:16:29 -0500

    avoid conflicts

--- test/dc_code.js
+++ test/dc_code.js
@@ -49,16 +49,7 @@ exports.testRelativePatterns = function(test) {
     [ 'section-with-word-section',
       'case shall be subject to the limitation set forth in [section 16-2326.01(b)(2)].',
       'section 16-2326.01(b)(2)',
-      '16', '2326.01', ['b', '2']],
-
-    // in § 28:9-324 of the DC Code 
-    [ 'UCC test case',
-      '(2) In all other cases, § 28:9-322(a) applies to the qualifying security interests.',
-      '§ 28:9-322(a)',
-      '28', '9-322', ['a']]
-    
-    // § 28:2A-530
-    
+      '16', '2326.01', ['b', '2']]
   ];
 
   for (var i=0; i<cases.length; i++) {
