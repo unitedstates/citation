950008141355e5233a0334767552ed614786d376
Eric Mill <eric@konklone.com>
2014-02-10 01:02:15 -0500

    ditch some comments

--- filters/lines.js
+++ filters/lines.js
@@ -3,26 +3,12 @@
   if (typeof Citation !== 'undefined' && Citation.filters) Citation.filters.lines = def;
 })({
 
-  // This function gets fed in whole text, any filter options,
-  // and a callback, `extract`.
-  // It should execute `extract` with each piece of text,
-  // and any associated metadata that should be merged with
-  // any cites extracted from that text.
   from: function(text, options, extract) {
     var delimiter = (options && options.delimiter) || /[\n\r]+/;
     var lines = text.split(new RegExp(delimiter));
     lines.forEach(function(line, i) {
       extract(line, {line: (i+1)});
     });
-  },
-
-  // take a cite that got extracted from a piece sent in above,
-  // do any post-processing on it.
-  //
-  // metadata will be {line: i} from above, just plop it on
-  // then: function(cite, metadata) {
-  //   cite.line = metadata.line;
-  //   return cite;
-  // }
+  }
 
 });
