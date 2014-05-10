7238e46e5311ebb50259018712dbce5c035b36ac
Eric Mill <eric@konklone.com>
2014-05-10 11:30:05 -0400

    annotate line filter

--- filters/lines.js
+++ filters/lines.js
@@ -1,5 +1,15 @@
 module.exports = {
 
+  /*
+    Filters receive:
+      * text: the entire input text
+      * options: any filter-specific options, e.g. delimiter
+      * extract: execute this function once with every substring the filter
+          breaks the input text into, e.g. each line,
+          along with any associated metadata, e.g. the line number.
+
+  */
+
   // A line-by-line filter.
   //
   // Breaks the text up by line, and feeds each line into the extractor.
@@ -10,8 +20,13 @@ module.exports = {
   //   delimiter: override the default delimiter
 
   from: function(text, options, extract) {
+    // by default, break lines on any combination of \n\r
     var delimiter = (options && options.delimiter) || /[\n\r]+/;
+
+    // split the text into an array of lines
     var lines = text.split(new RegExp(delimiter));
+
+    // for each line, submit it to the extractor along with its line number
     lines.forEach(function(line, i) {
       extract(line, {line: (i+1)});
     });
