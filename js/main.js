f3156477256c8f75a11034d31c7da071d2072268
Tom MacWright <tom@macwright.org>
2014-05-10 15:39:53 -0400

    Update website with live demo

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+var Citation = require('citation');
 
+var demoInput = document.getElementById('demo-input');
+var demoOutput = document.getElementById('demo-output');
+demoInput.onkeyup = update;
+
+update();
+
+var ex = document.getElementsByClassName('ex');
+
+for (var i = 0; i < ex.length; i++) {
+    ex[i].onclick = function() {
+        demoInput.value = this.dataset.ex;
+        update();
+        for (var i = 0; i < ex.length; i++) {
+            if (ex[i] !== this) {
+                ex[i].classList.add('active');
+            } else {
+                ex[i].classList.remove('active');
+            }
+        }
+    };
+}
+
+ex[0].onclick();
+
+function update() {
+    demoOutput.innerHTML = JSON.stringify(Citation.find(demoInput.value), null, 2);
+}
