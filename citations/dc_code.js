f87baaef33d44f0c8907b64d02df30c89886723f
Joshua Tauberer <jt@occams.info>
2014-09-13 15:27:25 -0400

    refactor dc_code citator to reduce code redundancy

--- citations/dc_code.js
+++ citations/dc_code.js
@@ -1,3 +1,9 @@
+var base_regex =
+  "(\\d+A?)" + // title
+  "\\s?\\-\\s?" + // dash
+  "([\\w\\d]+(?:\\.?[\\w\\d]+)?)" +  // section identifier (letters/numbers/dots)
+  "((?:\\([^\\)]+\\))*)"; // subsection (any number of adjacent parenthesized subsections)
+
 module.exports = {
   type: "regex",
 
@@ -12,77 +18,48 @@ module.exports = {
   parents_by: "subsections",
 
   patterns: function(context) {
-    // only apply this regex if we're confident that relative citations refer to the DC Code
-    if (context.source == "dc_code") {
-      return [
+    // D.C. Official Code 3-1202.04
+    // D.C. Official Code § 3-1201.01
+    // D.C. Official Code §§ 38-2602(b)(11)
+    // D.C. Official Code § 3- 1201.01
+    // D.C. Official Code § 3 -1201.01
+    //
+    // § 32-701
+    // § 32-701(4)
+    // § 3-101.01
+    // § 1-603.01(13)
+    // § 1- 1163.33
+    // § 1 -1163.33
+    // section 16-2326.01
 
-        // § 32-701
-        // § 32-701(4)
-        // § 3-101.01
-        // § 1-603.01(13)
-        // § 1- 1163.33
-        // § 1 -1163.33
-        // section 16-2326.01
-        {
-          regex:
-            "(?:section(?:s)?|§+)\\s+(\\d+A?)" +
-            "\\s?\\-\\s?" +
-            "([\\w\\d]+(?:\\.?[\\w\\d]+)?)" +  // section identifier, letters/numbers/dots
-            "((?:\\([^\\)]+\\))*)", // any number of adjacent parenthesized subsections
-
-          fields: ["title", "section", "subsections"],
-
-          processor: function(captures) {
-            var title = captures.title;
-            var section = captures.section;
-            var subsections = [];
-            if (captures.subsections)
-              subsections = captures.subsections.split(/[\(\)]+/).filter(function(x) {return x});
-
-            return {
-              title: title,
-              section: section,
-              subsections: subsections
-            };
-          }
-        }
-      ];
+    var prefix_regex = "";
+    var section_regex = "(?:sections?|§+)\\s+";
+    if (context.source != "dc_code") {
+      // Require "DC Official Code" but then make the section symbol optional.
+      prefix_regex = "D\\.?C\\.? (?:Official )?Code\\s+";
+      section_regex = "(?:" + section_regex + ")?";
     }
 
-    // absolute cites
-    else {
-      return [
+    return [
+      {
+        regex: prefix_regex + section_regex + base_regex,
 
-        // D.C. Official Code 3-1202.04
-        // D.C. Official Code § 3-1201.01
-        // D.C. Official Code §§ 38-2602(b)(11)
-        // D.C. Official Code § 3- 1201.01
-        // D.C. Official Code § 3 -1201.01
-        {
-          regex:
-            "D\\.?C\\.? (?:Official )?Code\\s+" + // absolute identifier
-            "(?:§+\\s+)?(\\d+A?)" +            // optional section sign, plus title
-            "\\s?\\-\\s?" +
-            "([\\w\\d]+(?:\\.?[\\w\\d]+)?)" +      // section identifier, letters/numbers/dots
-            "((?:\\([^\\)]+\\))*)", // any number of adjacent parenthesized subsections
+        fields: ["title", "section", "subsections"],
 
-          fields: ["title", "section", "subsections"],
+        processor: function(captures) {
+          var title = captures.title;
+          var section = captures.section;
+          var subsections = [];
+          if (captures.subsections)
+            subsections = captures.subsections.split(/[\(\)]+/).filter(function(x) {return x});
 
-          processor: function(captures) {
-            var title = captures.title;
-            var section = captures.section;
-
-            var subsections = [];
-            if (captures.subsections) subsections = captures.subsections.split(/[\(\)]+/).filter(function(x) {return x});
-
-            return {
-              title: title,
-              section: section,
-              subsections: subsections
-            };
-          }
+          return {
+            title: title,
+            section: section,
+            subsections: subsections
+          };
         }
-      ];
-    }
+      }
+    ];
   }
 };
