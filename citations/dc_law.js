549243502c015b020215026af7170e3ecf549a1c
Joshua Tauberer <jt@occams.info>
2014-09-13 13:42:22 -0400

    rather than not allowing 'DC' to appear in DC Law cites inside the dc_code context, allow it but make it optional (allowing <a> tags to cover the whole citation including the 'DC' part)

--- citations/dc_law.js
+++ citations/dc_law.js
@@ -7,10 +7,11 @@ module.exports = {
 
   patterns: function(context) {
     // If the context for this citation is the DC Code, then Law XX-YYY can be assumed
-    // to be a DC law. In other context, require the "DC Law" prefix.
-    var context_regex = "";
-    if (context.source != "dc_code")
-      context_regex = "D\\.?\\s*C\\.?\\s+";
+    // to be a DC law. In other context, require the "DC Law" prefix. In the DC Code
+    // context also slurp in the "DC" prefix.
+    var context_regex = "D\\.?\\s*C\\.?\\s+";
+    if (context.source == "dc_code")
+      context_regex = "(?:" + context_regex + ")?"
 
     return [
       // "D.C. Law 111-89"
